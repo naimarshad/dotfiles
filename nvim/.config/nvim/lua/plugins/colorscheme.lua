5857080b7a3f11bacdb319645f892cdbcaaa246c
Naeem Arshad <naimarshad@gmail.com>
2026-09-15 09:12:45 +0200

    feat(nvim): replace catppuccin with gruvbox dark colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,17 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "ellisonleao/gruvbox.nvim",
     priority = 1000,
     opts = {
-      flavour = "macchiato",
+      -- contrast "" is the standard gruvbox dark background (#282828), which is
+      -- what Ghostty's "Gruvbox Dark" theme uses. "hard" would be #1d2021.
+      contrast = "",
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "gruvbox",
     },
   },
 }
